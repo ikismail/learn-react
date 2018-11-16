e736d9cdaedc96f585b2e5ef28b2a155bd92cc0c
ikismail <ikismail7@gmail.com>
2018-11-16 15:56:50 +0530

    Code cleaning

--- src/App.js
+++ src/App.js
@@ -2,32 +2,36 @@ import React, { Component } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 class App extends Component {
-  /* State cannot be changed, state can be changed and if it changes and thats the special thing, it only works on that state property, If it changes it will lead react to re-render our dom or to update the dom, 
-    eg: if we change the name Mohammed Ismail this will lead to this being re-rendered
+  /* State cannot be changed, state can be changed and if it changes and thats
+  the special thing, it only works on that state property, If it changes
+  it will
+   lead react to re-render our dom or to update the dom,
+    eg: if we change the name Mohammed Ismail this will lead to this being
+    re-rendered
   */
   state = {
     persons: [
-      {id:"bzf1", name: "Mohammed Ismail", age: "24" },
-      {id:"bzf2", name: "Mohammed Ajmal", age: "23" }
+      { id: "bzf1", name: "Mohammed Ismail", age: "24" },
+      { id: "bzf2", name: "Mohammed Ajmal", age: "23" }
     ],
     showPersons: false
   };
 
   deletePersonHandler = index => {
     // const persons = this.state.persons.slice();
-    const persons = [...this.state.persons]
+    const persons = [...this.state.persons];
     persons.splice(index, 1);
     this.setState({ persons: persons });
   };
 
   nameChangeHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex( p => {
+    const personIndex = this.state.persons.findIndex(p => {
       return p.id === id;
-    })
+    });
 
     const person = {
       ...this.state.persons[personIndex]
-    }
+    };
 
     person.name = event.target.value;
 
@@ -35,8 +39,7 @@ class App extends Component {
 
     persons[personIndex] = person;
 
-    this.setState({persons: persons})
-
+    this.setState({ persons: persons });
   };
 
   togglePersons = () => {
@@ -44,7 +47,8 @@ class App extends Component {
     this.setState({ showPersons: !doesPersonShow });
   };
 
-  /* Every react component has to return or render some html code which can be rendered to the dom to the screen*/
+  /* Every react component has to return or render some html code which can
+  be rendered to the dom to the screen*/
   render() {
     // inline styles for components
     const style = {
@@ -56,11 +60,11 @@ class App extends Component {
 
     const classes = [];
 
-    if(this.state.persons.length <= 2){
-      classes.push('red')
+    if (this.state.persons.length <= 2) {
+      classes.push("red");
     }
-    if(this.state.persons.length <= 1){
-      classes.push('bold')
+    if (this.state.persons.length <= 1) {
+      classes.push("bold");
     }
 
     let persons = null;
@@ -71,11 +75,11 @@ class App extends Component {
         <div>
           {this.state.persons.map((person, index) => {
             return (
-              <Person 
+              <Person
                 key={person.id}
                 name={person.name}
                 age={person.age}
-                changed={(event) => this.nameChangeHandler(event, person.id)}
+                changed={event => this.nameChangeHandler(event, person.id)}
                 click={() => this.deletePersonHandler(index)}
               />
             );
@@ -83,7 +87,7 @@ class App extends Component {
           {/* <Person
             name={this.state.persons[0].name}
             age={this.state.persons[0].age}
-            
+
           >
             loves JS
           </Person>
@@ -95,7 +99,7 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = "red"
+      style.backgroundColor = "red";
     }
 
     return (
@@ -103,12 +107,9 @@ class App extends Component {
       <div className="App">
         <h1>This is Mohammed Ismail</h1>
 
-        <h6 className={classes.join(' ')}>Dynamic Class Example</h6>
+        <h6 className={classes.join(" ")}>Dynamic Class Example</h6>
 
-        <button
-          style={style}
-          onClick={this.togglePersons}
-        >
+        <button style={style} onClick={this.togglePersons}>
           {" "}
           Toggle Person
         </button>
@@ -123,11 +124,11 @@ class App extends Component {
     );
 
     // 👆 this jsx html will gets compiled to 👇 this code
-    /*  
+    /*
    return React.createElement('div', {
       className: 'App'
-    }, React.createElement('h1', null, 'This is Mohammed Ismail')) 
-    
+    }, React.createElement('h1', null, 'This is Mohammed Ismail'))
+
     */
   }
 }
